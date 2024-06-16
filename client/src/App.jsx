1e4ec46cae623536919d0f789d175baa88b4f554
Durgeshwar Sahani <84055021+dksahani2002@users.noreply.github.com>
2024-06-16 13:08:03 +0530

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/signin" element={<Signin />}/>
+      <Route path="/signup" element={<Signup />}/>
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
